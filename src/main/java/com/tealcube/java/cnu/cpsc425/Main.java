72a125812755a4725112bd116f18a6f95e37d7e9
Richard Harrah <topplethenunnery@gmail.com>
2015-04-14 23:23:16 -0400

    adding headers

--- src/main/java/com/tealcube/java/cnu/cpsc425/Main.java
+++ src/main/java/com/tealcube/java/cnu/cpsc425/Main.java
@@ -1,7 +1,20 @@
+/*
+ * This file is part of Facecore, licensed under the ISC License.
+ *
+ * Copyright (c) 2014 Richard Harrah
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
 package com.tealcube.java.cnu.cpsc425;
 
 import com.javafx.experiments.jfx3dviewer.Xform;
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
@@ -10,7 +23,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,15 +30,6 @@ public class Main extends Application {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     private Stage primaryStage;
-    private BorderPane base;
-    private SubScene subScene;
-    private Timeline timeline;
-    private boolean timelinePlaying = false;
-    private double ONE_FRAME = 1.0 / 24.0;
-    private double DELTA_MULTIPLIER = 200.0;
-    private double CONTROL_MULTIPLIER = 0.1;
-    private double SHIFT_MULTIPLIER = 0.1;
-    private double ALT_MULTIPLIER = 0.5;
     private double mousePosX, mousePosY, mouseOldX, mouseOldY, mouseDeltaX, mouseDeltaY;
     private final Group root3D = new Group();
     private final Group axisGroup = new Group();
@@ -37,7 +40,6 @@ public class Main extends Application {
     private final Xform cameraXform = new Xform();
     private final Xform cameraXform2 = new Xform();
     private final Xform cameraXform3 = new Xform();
-    private final double cameraDistance = 600;
 
     public static Logger getLogger() {
         return LOGGER;
@@ -55,11 +57,11 @@ public class Main extends Application {
         getLogger().debug("Application starting");
 
         FXMLLoader loader = new FXMLLoader();
-        base = loader.load(Main.class.getResourceAsStream("SceneEditor.fxml"));
+        BorderPane base = loader.load(Main.class.getResourceAsStream("SceneEditor.fxml"));
         SceneEditorController controller = loader.getController();
         controller.setMain(this);
 
-        subScene = new SubScene(world, 690, 600, true, SceneAntialiasing.BALANCED);
+        SubScene subScene = new SubScene(world, 690, 600, true, SceneAntialiasing.BALANCED);
         base.setCenter(subScene);
 
         buildScene();
@@ -70,7 +72,7 @@ public class Main extends Application {
 
         Scene scene = new Scene(base, 800, 600, true);
         scene.setFill(Color.ALICEBLUE);
-        handleMouse(scene, world);
+        handleMouse(scene);
 
         this.primaryStage.setScene(scene);
         this.primaryStage.show();
@@ -107,6 +109,7 @@ public class Main extends Application {
 
         camera.setNearClip(0.1);
         camera.setFarClip(10000.0);
+        double cameraDistance = 600;
         camera.setTranslateZ(-cameraDistance);
         cameraXform.ry.setAngle(320.0);
         cameraXform.rx.setAngle(40);
@@ -152,7 +155,7 @@ public class Main extends Application {
         return houseGroup;
     }
 
-    private void handleMouse(Scene scene, final Node root) {
+    private void handleMouse(Scene scene) {
         scene.setOnMousePressed(me -> {
             mousePosX = me.getSceneX();
             mousePosY = me.getSceneY();
